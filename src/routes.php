74539c3c08a11138f644528528eadae64da9e15a
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-01 20:22:23 +0300

    Update

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/tgwebhook', 'GarbuzIvan\TelegramBot\Controllers\Web@webhook');
+Route::get('/tgwebhook', '\GarbuzIvan\TelegramBot\Controllers\Web@webhook');
