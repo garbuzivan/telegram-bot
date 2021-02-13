6e2209cabad3dea21f4875f4be1a93b344792379
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-13 14:31:58 +0300

    Update

---
+++ src/Commands/Help.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GarbuzIvan\TelegramBot\Commands;
+
+class Help extends Start
+{
+    /**
+     * @var string Command Name
+     */
+    protected $name = "help";
+}
--- src/Commands/Start.php
+++ src/Commands/Start.php
@@ -17,7 +17,7 @@ class Start extends Command
     /**
      * @var string Command Description
      */
-    protected $description = "Start Command to get you started";
+    protected $description = "Список команд";
 
     /**
      * @inheritdoc
@@ -28,7 +28,7 @@ class Start extends Command
         // the user/chat id who triggered this command.
         // `replyWith<Message|Photo|Audio|Video|Voice|Document|Sticker|Location|ChatAction>()` all the available methods are dynamically
         // handled when you replace `send<Method>` with `replyWith` and use the same parameters - except chat_id does NOT need to be included in the array.
-        $this->replyWithMessage(['text' => 'Hello! Welcome to our bot, Here are our available commands:']);
+        $this->replyWithMessage(['text' => 'Император ЯН приветствует тебя']);
 
         // This will update the chat status to typing...
         $this->replyWithChatAction(['action' => Actions::TYPING]);
