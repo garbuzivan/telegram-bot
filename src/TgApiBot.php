39b7d39f71fb56798d05d4c7ab8467e194e944a0
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-13 14:07:01 +0300

    Update

--- src/TgApiBot.php
+++ src/TgApiBot.php
@@ -18,7 +18,7 @@ class TgApiBot extends \Telegram\Bot\Api
     {
         $message = parent::sendMessage($params);
         $parser = new ParserBot();
-        $parser->newMessage((array)$message, 0);
+        $parser->newMessage((array)$message, 1);
         file_put_contents(public_path('message.php'), json_encode($message) . "\n\n", 8);
         return $message;
     }
