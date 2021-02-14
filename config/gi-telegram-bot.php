46f0c54fdb959141fe86d239e7d4baf875a46893
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-14 11:25:59 +0300

    Update

--- config/gi-telegram-bot.php
+++ config/gi-telegram-bot.php
@@ -6,9 +6,17 @@ return [
 
     // Auth methods
     'commands' => [
-        \GarbuzIvan\TelegramBot\Commands\Start::class,
+        \GarbuzIvan\TelegramBot\Commands\All::class,
+        \GarbuzIvan\TelegramBot\Commands\Bonus::class,
         \GarbuzIvan\TelegramBot\Commands\Help::class,
+        \GarbuzIvan\TelegramBot\Commands\Inventory::class,
         \GarbuzIvan\TelegramBot\Commands\Rank::class,
+        \GarbuzIvan\TelegramBot\Commands\Sex::class,
+        \GarbuzIvan\TelegramBot\Commands\Shop::class,
+        \GarbuzIvan\TelegramBot\Commands\Start::class,
+        \GarbuzIvan\TelegramBot\Commands\View::class,
+        \GarbuzIvan\TelegramBot\Commands\Where::class,
+        \GarbuzIvan\TelegramBot\Commands\Who::class,
     ],
 
 ];
