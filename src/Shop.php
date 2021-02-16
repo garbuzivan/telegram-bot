61c17bc8a25a0950ce218dfcd7877b0dee0a8c41
Garbuz Ivan <yanzlatov@gmail.com>
2021-02-16 17:28:44 +0300

    Update

---
+++ src/Shop.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GarbuzIvan\TelegramBot;
+
+class Shop
+{
+    public function userPay(int $itemID, int $userID): bool
+    {
+
+        return true;
+    }
+}
